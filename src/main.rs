0de629f6f2d865b68a4ee826aa99d4fdc5d8780f
Damian J Pound <damian@thebestisp.com>
2022-04-16 20:16:47 -0500

    Set to final scene

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ pub mod vec;
 
 use crate::{
     camera::Camera,
-    material::{Dielectric, Lambertian, Metal},
+    material::{Dielectric, Lambertian, Material, Metal},
 };
 use hit::Hittable;
 use rand::random;
@@ -32,49 +32,83 @@ fn ray_color(r: Ray, world: &[Box<dyn Hittable>], depth: u32) -> Vec3<f64> {
     Vec3(1.0, 1.0, 1.0) * (1.0 - t) + Vec3(0.5, 0.7, 1.0) * t
 }
 
-fn main() {
-    // Image
-    const ASPECT_RATIO: f64 = 16. / 9.;
-    const IMAGE_WIDTH: u32 = 800;
-    const IMAGE_HEIGHT: u32 = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as u32;
-    const SAMPLES_PER_PIXEL: usize = 100;
-    const MAX_DEPTH: u32 = 50;
+fn random_scene() -> Vec<Box<dyn Hittable>> {
+    let mut world: Vec<Box<dyn Hittable>> = Vec::new();
 
-    // World
-    let material_ground = Arc::new(Lambertian {
-        albedo: Vec3(0.8, 0.8, 0.),
+    let ground_material = Arc::new(Lambertian {
+        albedo: Vec3(0.5, 0.5, 0.5),
     });
-    let material_center = Arc::new(Lambertian {
-        albedo: Vec3(0.1, 0.2, 0.5),
-    });
-    let material_left = Arc::new(Dielectric { ir: 1.5 });
-    let material_left2 = material_left.clone();
-    let material_right = Arc::new(Metal {
+    world.push(Box::new(Sphere::new(
+        Vec3(0., -1000., 0.),
+        1000.,
+        ground_material,
+    )));
+
+    Arc::new(Dielectric { ir: 1.5 });
+    Arc::new(Metal {
         albedo: Vec3(0.8, 0.6, 0.2),
         fuzz: 0.,
     });
 
-    let world: Vec<Box<dyn Hittable>> = vec![
-        Box::new(Sphere::new(Vec3(0.0, -100.5, -1.0), 100.0, material_ground)),
-        Box::new(Sphere::new(Vec3(0.0, 0.0, -1.0), 0.5, material_center)),
-        Box::new(Sphere::new(Vec3(-1.0, 0.0, -1.0), 0.5, material_left)),
-        Box::new(Sphere::new(Vec3(-1.0, 0.0, -1.0), -0.45, material_left2)),
-        Box::new(Sphere::new(Vec3(1.0, 0.0, -1.0), 0.5, material_right)),
-    ];
+    for a in -11..11 {
+        for b in -11..11 {
+            let center = Vec3(
+                (a as f64) + 0.9 * random::<f64>(),
+                0.2,
+                (b as f64) + 0.9 * random::<f64>(),
+            );
+            if (center - Vec3(4., 0.2, 0.)).length() <= 0.9 {
+                continue;
+            }
+
+            let choose_mat: f32 = random();
+            let mat: Arc<dyn Material> = if choose_mat < 0.8 {
+                Arc::new(Lambertian {
+                    albedo: Vec3::random(0. ..1.) * Vec3::random(0. ..1.),
+                })
+            } else if choose_mat < 0.95 {
+                Arc::new(Metal {
+                    albedo: Vec3::random(0.5..1.),
+                    fuzz: random::<f64>() * 0.5,
+                })
+            } else {
+                Arc::new(Dielectric { ir: 1.5 })
+            };
+            world.push(Box::new(Sphere::new(center, 0.2, mat)));
+        }
+    }
+
+    let material1 = Arc::new(Dielectric { ir: 1.5 });
+    world.push(Box::new(Sphere::new(Vec3(0., 1., 0.), 1.0, material1)));
+
+    let material2 = Arc::new(Lambertian {
+        albedo: Vec3(0.4, 0.2, 0.1),
+    });
+    world.push(Box::new(Sphere::new(Vec3(-4., 1., 0.), 1.0, material2)));
+
+    let material3 = Arc::new(Metal {
+        albedo: Vec3(0.7, 0.6, 0.5),
+        fuzz: 0.0,
+    });
+    world.push(Box::new(Sphere::new(Vec3(4., 1., 0.), 1.0, material3)));
+    world
+}
+fn main() {
+    // Image
+    const ASPECT_RATIO: f64 = 3. / 2.;
+    const IMAGE_WIDTH: u32 = 1200;
+    const IMAGE_HEIGHT: u32 = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as u32;
+    const SAMPLES_PER_PIXEL: usize = 500;
+    const MAX_DEPTH: u32 = 50;
+
+    // World
+    let world = random_scene();
 
     // Camera
-    let look_from = Vec3(3., 3., 2.);
-    let look_at = Vec3(0., 0., -1.);
+    let look_from = Vec3(13., 2., 3.);
+    let look_at = Vec3(0., 0., 0.);
     let vup = Vec3(0., 1., 0.);
-    let cam = Camera::new(
-        look_from,
-        look_at,
-        vup,
-        20.,
-        ASPECT_RATIO,
-        2.,
-        (look_from - look_at).length(),
-    );
+    let cam = Camera::new(look_from, look_at, vup, 20., ASPECT_RATIO, 0.1, 10.);
 
     // Render
     println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
